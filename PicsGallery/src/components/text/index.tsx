1f26f85bbfe08e2b75a47a35155f5b889e5ba48d
Manolo Sainas <sainas.manolo@gmail.com>
2025-06-26 12:35:06 +0200

    Added the camera icon (favicon)

--- PicsGallery/src/components/text/index.tsx
+++ PicsGallery/src/components/text/index.tsx
@@ -4,7 +4,7 @@ import { StyledText } from './styled'
 type TextType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span'
 
 type Props = PropsWithChildren<{
-  as?: TextType
+  as?: TextType // special prop, changes the type of HTML element
 }>
 
 export const Text = ({ as = 'h1', children }: Props) => {
--- PicsGallery/src/components/text/styled.ts
+++ PicsGallery/src/components/text/styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const StyledText = styled.h1`
   color: green;
-  text-align: center;
+
   /* @media (max-width: 768px) {
     color: green;
   } */
